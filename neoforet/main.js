8e39fb92cf7eb9fcc292e05fc18ec9e4b1ff14ae
seyoungKim <sagekim6@gmail.com>
2022-03-28 18:41:18 +0900

    22.03.28-footer styling and main contents JS by sagekim

--- neoforet/main.js
+++ neoforet/main.js
@@ -1,22 +1,53 @@
 const silder = document.querySelector("#slider");
+const bullets = document.querySelectorAll(".bullet");
 const swiper = document.querySelector(".swiper");
-const bullets = swiper.querySelectorAll(".bullet");
+const goUpBtn = document.querySelector(".goUpBtn");
+const header = document.querySelector("header");
 
+const backgroundImages = [
+  "url('https://www.neoforet.co.kr/resources/static/common/images/main/bg1.jpg')",
+  "url('https://www.neoforet.co.kr/resources/static/common/images/main/bg2.jpg')",
+  "url('https://www.neoforet.co.kr/resources/static/common/images/main/bg3.jpg')",
+];
 let imgIndex = 0;
 
-// 클릭 시 배경이미지 변경 함수
-function clickToMoveImg(event) {}
-// 1. 불렛을 클릭하면 배경 이미지가 바껴야한다
-// 2. setInterval에 맞춰서 불렛도 같이 순서대로 변경되어야한다
-
-// 배경이미지 자동 변경 함수
+// 배경이미지 자동 변경
 function automaticImageSlide() {
-  silder.style.backgroundImage = bgImages[imgIndex];
+  silder.style.backgroundImage = backgroundImages[imgIndex];
+  if (imgIndex === 0) {
+    bullets[2].style.backgroundColor = "transparent";
+    bullets[0].style.backgroundColor = "rgb(255 255 255 / 50%)";
+  } else if (imgIndex === 1) {
+    bullets[0].style.backgroundColor = "transparent";
+    bullets[1].style.backgroundColor = "rgb(255 255 255 / 50%)";
+  } else if (imgIndex === 2) {
+    bullets[1].style.backgroundColor = "transparent";
+    bullets[2].style.backgroundColor = "rgb(255 255 255 / 50%)";
+  }
   imgIndex++;
-  if (imgIndex > bgImages.length) {
+  if (imgIndex > backgroundImages.length) {
     imgIndex = 0;
   }
 }
 setInterval(automaticImageSlide, 5000);
 
-swiper.addEventListener("click", clickToMoveImg);
+// 클릭 시 배경이미지 변경
+function moveToClickedImg(event) {
+  if (event.target.classList.contains("dot-one")) {
+    silder.style.backgroundImage = backgroundImages[0];
+    bullets[0].style.backgroundColor = "rgb(255 255 255 / 50%)";
+  } else if (event.target.classList.contains("dot-two")) {
+    silder.style.backgroundImage = backgroundImages[1];
+    bullets[1].style.backgroundColor = "rgb(255 255 255 / 50%)";
+  } else if (event.target.classList.contains("dot-three")) {
+    silder.style.backgroundImage = backgroundImages[2];
+    bullets[2].style.backgroundColor = "rgb(255 255 255 / 50%)";
+  }
+}
+
+swiper.addEventListener("click", moveToClickedImg);
+
+// 페이지 상단으로 이동
+function moveToTop() {
+  document.body.scrollIntoView({ behavior: "smooth" });
+}
